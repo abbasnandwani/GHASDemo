125e5817a7a0c00fec4bd440468cdd7ead3f5f69
Abbas N <anandwani@microsoft.com>
2023-10-11 23:24:56 +0100

    check

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            string sql = "SELECT * FROM Users WHERE Id = " + args[0];
         }
     }
 }
